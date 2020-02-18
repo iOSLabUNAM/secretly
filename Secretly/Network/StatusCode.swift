e42940f1cea3430a1682427a823feec8fdf1a062
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-02-17 21:36:38 -0600

    align case statements

--- Secretly/Network/StatusCode.swift
+++ Secretly/Network/StatusCode.swift
@@ -18,19 +18,19 @@ enum StatusCode: Int {
 
     public init(rawValue: Int) {
         switch rawValue {
-            case 100, 101, 102:
-                self = .info
-            case 200, 201, 202, 203, 204, 205, 206, 207, 208, 226:
-                self = .success
-            case 300, 301, 302, 303, 304, 305, 306, 307, 308:
-                self = .redirection
-            case 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412,
-                 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 451:
-                self = .clientError
-            case 500, 501, 502, 503, 504, 505, 506, 507, 510, 511:
-                self = .serverError
-            default:
-                self = .unkown
+        case 100, 101, 102:
+            self = .info
+        case 200, 201, 202, 203, 204, 205, 206, 207, 208, 226:
+            self = .success
+        case 300, 301, 302, 303, 304, 305, 306, 307, 308:
+            self = .redirection
+        case 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412,
+             413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 451:
+            self = .clientError
+        case 500, 501, 502, 503, 504, 505, 506, 507, 510, 511:
+            self = .serverError
+        default:
+            self = .unkown
         }
     }
 }
