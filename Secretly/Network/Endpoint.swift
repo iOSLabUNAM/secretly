0bead7fa1bd2a04cb2ffa80f3b06b96854d0cbfd
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-10-22 15:10:31 -0500

    Add missing methods to endpoint

--- Secretly/Network/Endpoint.swift
+++ Secretly/Network/Endpoint.swift
@@ -29,9 +29,47 @@ struct Endpoint<T> where T: Codable {
     }
     typealias EndpointResponse = (Result<T?, Error>) -> Void
 
+    func all(completion: @escaping (Result<[T]?, Error>) -> Void) {
+        client.get(path: path) { response in
+            let serializedResponse = response.flatMap { serializeItemList($0) }
+            DispatchQueue.main.async { completion(serializedResponse) }
+        }
+    }
+
     func find(id: String = "", completion: @escaping EndpointResponse) {
         client.get(path: "\(path)/\(id)") { response in
             let serializedResponse = response.flatMap { serializeItem($0) }
+            DispatchQueue.main.async { completion(serializedResponse) }
+        }
+    }
+
+    func create(codable: T, completion: @escaping EndpointResponse) {
+        guard let data = try? encoder.encode(codable) else {
+            completion(.failure(SerializingError()))
+            return
+        }
+        client.post(path: path, body: data) { response in
+            let serializedResponse = response.flatMap { serializeItem($0) }
+            DispatchQueue.main.async { completion(serializedResponse) }
+        }
+    }
+
+    func update(id: String, codable: T, completion: @escaping EndpointResponse) {
+        guard let data = try? encoder.encode(codable) else {
+            completion(.failure(SerializingError()))
+            return
+        }
+        client.put(path: "\(path)/\(id)", body: data) { response in
+            let serializedResponse = response.flatMap { serializeItem($0) }
+            DispatchQueue.main.async { completion(serializedResponse) }
+        }
+    }
+
+    func destroy(id: String, completion: @escaping EndpointResponse) {
+        client.delete(path: "\(path)/\(id)") { response in
+            let serializedResponse = response.flatMap { _ -> Result<T?, Error> in
+                return .success(nil)
+            }
             DispatchQueue.main.async { completion(serializedResponse) }
         }
     }
@@ -42,6 +80,22 @@ struct Endpoint<T> where T: Codable {
         }
         do {
             let json = try decoder.decode(T.self, from: data)
+            return .success(json)
+        } catch let err {
+            #if DEBUG
+            debugPrint(err)
+            debugPrint(String(data: data, encoding: .utf8) ?? "")
+            #endif
+            return .failure(SerializingError())
+        }
+    }
+
+    private func serializeItemList(_ data: Data?) -> Result<[T]?, Error> {
+        guard let data = data else {
+            return .failure(EmptyResponseError())
+        }
+        do {
+            let json = try decoder.decode([T].self, from: data)
             return .success(json)
         } catch let err {
             #if DEBUG
