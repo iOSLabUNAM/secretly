86348b558d966827b85e4eb9bcae93a9bb47396f
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-10-25 12:22:32 -0600

    Implement auth custom encoding/decoding

--- Secretly/Models/AuthUser.swift
+++ Secretly/Models/AuthUser.swift
@@ -12,4 +12,26 @@ struct AuthUser: Codable {
     let username: String?
     let password: String?
     let token: String?
+    
+    private enum EncodingKeys: String, CodingKey {
+        case username
+        case password
+    }
+    
+    private enum DecodingKeys: String, CodingKey {
+        case token
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DecodingKeys.self)
+        self.token = try container.decode(String?.self, forKey: .token)
+        self.username = nil
+        self.password = nil
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: EncodingKeys.self)
+        try container.encode(String(self.username ?? ""), forKey: .username)
+        try container.encode(String(self.password ?? ""), forKey: .password)
+    }
 }
