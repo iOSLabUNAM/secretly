4f2077201c75ae916d6ee4146ee1f9ddf71bb9e5
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2021-05-22 14:45:28 -0500

    It implements network layer (#15)

---
+++ Secretly/Network/RequestError.swift
@@ -0,0 +1,13 @@
+//
+//  RequestError.swift
+//  Secretly
+//
+//  Created by Luis Ezcurdia on 22/05/21.
+//  Copyright © 2021 3zcurdia. All rights reserved.
+//
+
+import Foundation
+
+enum RequestError: Error {
+    case invalidRequest
+}
