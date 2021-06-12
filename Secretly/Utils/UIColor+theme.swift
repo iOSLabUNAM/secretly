5d79d7916b12c25bf866b4ac2c35cd1368dbebd1
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2021-06-12 12:22:19 -0500

    Implement post creation and refactor (#30)

---
+++ Secretly/Utils/UIColor+theme.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+theme.swift
+//  Secretly
+//
+//  Created by Luis Ezcurdia on 11/06/21.
+//  Copyright © 2021 3zcurdia. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let text = UIColor(named: "Black chocolate")!
+    static let tint = UIColor(named: "Champagne pink")
+    static let foreground = UIColor(named: "Yale blue")!
+    static let background = UIColor(named: "Cadet gray")!
+}
