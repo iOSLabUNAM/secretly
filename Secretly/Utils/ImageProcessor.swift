0b53973f90d1b9f24d100f132304ffa08818ea12
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2021-06-28 14:54:21 -0500

    Improve core image filter implementation (#40)

--- Secretly/Utils/ImageProcessor.swift
+++ Secretly/Utils/ImageProcessor.swift
@@ -16,10 +16,10 @@ struct ImageProcessor {
     func process(_ image: UIImage, completion: @escaping (UIImage) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
             guard let resized = resize(image, for: CGSize(width: width, height: heigth)),
-                  let result  = blur(resized, radius: 25)
+                  let result  = blur(resized, radius: 30)
                   else { return }
             DispatchQueue.main.async {
-                completion(UIImage(ciImage: result))
+                completion(UIImage(cgImage: result))
             }
         }
     }
@@ -32,11 +32,19 @@ struct ImageProcessor {
         return CIImage(image: resized)
     }
 
-    private func blur(_ image: CIImage?, radius: CGFloat) -> CIImage? {
+    private func blur(_ image: UIImage?, radius: CGFloat) -> CGImage? {
+        guard let image = image else { return nil }
+        return blur(CIImage(image: image), radius: radius)
+    }
+
+    private func blur(_ image: CIImage?, radius: CGFloat) -> CGImage? {
         guard let input = image,
-              let filter = CIFilter(name: "CIGaussianBlur") else { return nil }
+              let filter = CIFilter(name: "CIHexagonalPixellate") else { return nil }
+        let context = CIContext()
         filter.setValue(input, forKey: kCIInputImageKey)
-        filter.setValue(radius, forKey: kCIInputRadiusKey)
-        return filter.outputImage
+        filter.setValue(CIVector(x: width/2, y: heigth/2), forKey: kCIInputCenterKey)
+        filter.setValue(radius, forKey: kCIInputScaleKey)
+        guard let result = filter.outputImage else { return nil }
+        return context.createCGImage(result, from: result.extent)
     }
 }
