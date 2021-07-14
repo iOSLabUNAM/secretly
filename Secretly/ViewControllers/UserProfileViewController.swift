623424eae387cc8496447222dce4239c4314cb20
Yocelin Garcia <garcia_romero.y@hotmail.com>
2021-07-14 01:25:45 -0500

    Load user profile view and conect with user profile service

--- Secretly/ViewControllers/UserProfileViewController.swift
+++ Secretly/ViewControllers/UserProfileViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class UserProfileViewController: UIViewController {
+    
+    @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var userProfileName: UILabel!
+    
     let service = UserProfileService()
-
+    var user: User? {
+        didSet {
+            setUserView()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-        backgroundImage.image = UIImage(named: "background")
+        backgroundImage.image = UIImage(named: "background-green")
         backgroundImage.contentMode = .scaleAspectFill
         view.insertSubview(backgroundImage, at: 0)
+        loadUserProgile()
+    }
+    
+    @objc
+    func loadUserProgile() {
+        service.showProfile { [unowned self] user in self.user = user }
+    }
+    
+    @objc func setUserView(){
+        userProfileImage.image = nil
+        
+        guard let user = user else { return }
+        
+        userProfileName.text = user.username
+        ImageLoader.load(user.avatarUrl) { [unowned self] img in self.userProfileImage.image = img }
     }
 }
