207a6c183938e217345b966de9e8f1c223d43f03
karen <karen_maya@cuaed.unam.mx>
2021-08-06 20:03:45 -0500

    Se implemento un servicio con las funciones de createLike y deleteLike para obtener el identificador y poder crear o borrar el like

--- Secretly/Services/LikeServices.swift
+++ Secretly/Services/LikeServices.swift
@@ -1,11 +1,10 @@
 import Foundation
+
 struct LikeService {
     private var endpoint: RestClient<Like>
-
     init() {
         endpoint = RestClient<Like>(client: AmacaConfig.shared.httpClient, path: "/api/v1/posts")
     }
-
     func createLike(postId:Int, completion: ((Result<Like?, Error>) -> Void)? = nil ) {
         let identifier = "\(postId)/likes"
         endpoint.createNoModel(identifier){
@@ -21,5 +20,5 @@ struct LikeService {
         }
     }
     
-    }
+}
 
