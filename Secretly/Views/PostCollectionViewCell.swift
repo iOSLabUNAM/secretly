e3c5c8ef6b2c35b15a7f60eb776081b60fcb72ce
rhodstar <rhodfra@gmail.com>
2021-08-26 14:59:00 -0500

    Change Like Image View into a Button
    Also added tap gesture to update like interface

--- Secretly/Views/PostCollectionViewCell.swift
+++ Secretly/Views/PostCollectionViewCell.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class PostCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "feedPostCell"
+    var likeService: LikeService?
     var post: Post? {
         didSet {
            updateView()
+            likeService = LikeService(post: post)
         }
     }
     @IBOutlet weak var authorView: AuthorView!
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var likeState: UIImageView!
+    @IBOutlet weak var likeState: UIButton!
     @IBOutlet weak var commentCounter: UILabel!
-
+   
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -37,5 +39,25 @@ class PostCollectionViewCell: UICollectionViewCell {
             ImageLoader.load(postImg.mediumUrl) { img in self.imageView.image = img }
         }
         self.authorView.author = post.user
+        
+        let imageName = post.liked ?? false ? "heart.fill" : "heart"
+        likeState.setImage(UIImage(systemName: imageName), for: .normal)
+        
+    }
+    
+    @IBAction func likeOnTap(_ sender: UIButton) {
+        likeService?.action{ [unowned self] result in
+            var imageName = "heart"
+            switch result {
+            case .success(nil):
+                post?.toggleLike(state: "dec")
+            case .success:
+                imageName = "heart.fill"
+                post?.toggleLike(state: "inc")
+            case .failure:
+                print("No se pudo actualizar el like")
+            }
+            likeState.setImage(UIImage(systemName: imageName ),for: .normal)
+        }
     }
 }
